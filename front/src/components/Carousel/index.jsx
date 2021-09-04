ae234947dc9fdb5b07bfa442891def03ff2e3527
Felipe Queiroz Bueno <lipemotog4plus@gmail.com>
2021-09-04 15:14:14 -0300

    update carousel

--- front/src/components/Carousel/index.jsx
+++ front/src/components/Carousel/index.jsx
@@ -19,14 +19,86 @@ export default function Carousel(){
     return(
             <Container>
                 <div className="owl-carousel owl-theme">
-                    <div className="item"><img src={Anfield} alt="" /></div>
-                    <div className="item"><img src={Beach} alt="" /></div>
-                    <div className="item"><img src={Cableway} alt="" /></div>
-                    <div className="item"><img src={Cover} alt="" /></div>
-                    <div className="item"><img src={Coliseum} alt="" /></div>
-                    <div className="item"><img src={Island} alt="" /></div>
+                    <div className="item">
+                        <figure>
+                            <img src={Anfield} alt="" />
+                            <figcaption>
+                                <h2>Conheça Anfield</h2>
+                                <p>
+                                    O Estádio Anfield é um estádio localizado em Liverpool, Inglaterra. 
+                                    É a casa do time de futebol Liverpool FC 
+                                    e um dos estádios considerados 5 Estrelas pela UEFA.
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className="item">
+                        <figure>
+                            <img src={Beach} alt="" />
+                            <figcaption>
+                                <h2> Praia em Ilhas tropicais </h2>
+                                <p>
+                                Visite praias em ilhas tropicais em um lugar secreto. Você descobre o local só quando chegamos lá.
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className="item">
+                        <figure>
+                            <img src={Cableway} alt="" />
+                            <figcaption>
+                                <h2> Pacote Santiago + Vale Nevado </h2>
+                                <p>
+                                    Aproveite o Chile, um país com muita diversidade e montanhas cobertas de gelo. Pacote Incluí o Vale Nevado em alta temporada
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className="item">
+                        <figure>
+                            <img src={Cover} alt="" />
+                            <figcaption>
+                                <h2> Praia </h2>
+                                <p>
+                                    Temos muitos pacotes com boas praias
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className="item">
+                        <figure>
+                            <img src={Coliseum} alt="" />
+                            <figcaption>
+                                <h2> Explore a cidade de Roma </h2>
+                                <p>
+                                    Roma, a capital da Itália, é uma cidade cosmopolita, enorme, com quase 3.000 anos de arte, arquitetura e cultura influentes no mundo todo e à mostra.
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className="item">
+                        <figure>
+                            <img src={Louvre} alt="" />
+                            <figcaption>
+                                <h2> Museu do Louvre </h2>
+                                <p>
+                                    Conheça o maior museu de arte do mundo e um monumento histórico em Paris.
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className="item">
+                        <figure>
+                            <img src={Island} alt="" />
+                            <figcaption>
+                                <h2> Praias paradisíacas em ilhas desertas </h2>
+                                <p>
+                                Desfrute dessa experiência de conhecer praias paradisíacas em olhas desertas próximas ao Brasil
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
                     <div className="item"><img src={Lake} alt="" /></div>
-                    <div className="item"><img src={Louvre} alt="" /></div>
                     <div className="item"><img src={Montain} alt="" /></div>
                     <div className="item"><img src={Road} alt="" /></div>
                     <div className="item"><img src={Tokyo} alt="" /></div>
--- front/src/components/Carousel/style.js
+++ front/src/components/Carousel/style.js
@@ -8,4 +8,24 @@ export const Container = styled.div`
         width: 300px;
         height: 200px;
     }
+
+    figcaption{
+        position: absolute;
+        top: 0px;
+        background-color: rgba(0,0,0,0.4);
+        width: 100%;
+        height: 95%;
+        box-sizing: border-box;
+        color: whitesmoke;
+
+        opacity: 0;
+    }
+
+    figure:hover figcaption {
+        opacity: 1;
+    }
+
+    h2{
+        padding-bottom: 30px;
+    }
 `;
